7fd93860baf3b0c70801c002284ff1bf6acd2047
Wenzel <wenzel@buho.la>
2026-07-22 12:06:58 -0500

    Fix |vendeya| Estilos y otras correciones de config

---
+++ public/vendeya/assets/app.87e2d48f.js
@@ -0,0 +1,46 @@
+import _sfc_main$1 from "./AppLayout.4ed3f60c.js";
+import { b as defineComponent, a5 as useRoute, x as resolveComponent, f as openBlock, v as createBlock, B as withCtx, w as createVNode, T as Transition, s as resolveDynamicComponent, Z as unref } from "./vendor.b09305a7.js";
+import "./VButton.fc4cbeca.js";
+import "./plugin-vue_export-helper.5a098b48.js";
+import "./VIconButton.d7e06796.js";
+import "./index.0eef4411.js";
+import "./IsotipoMozoOficial.6fcf915b.js";
+import "./VModal.ff5b02d6.js";
+import "./VControl.bc7d1930.js";
+import "./VField.6fe855fb.js";
+import "./VDropdown.6720ff68.js";
+import "./masterService.6c6be1bd.js";
+var block0 = {};
+const _sfc_main = /* @__PURE__ */ defineComponent({
+  setup(__props) {
+    const route = useRoute();
+    return (_ctx, _cache) => {
+      const _component_RouterView = resolveComponent("RouterView");
+      const _component_AppLayout = _sfc_main$1;
+      return openBlock(), createBlock(_component_AppLayout, null, {
+        default: withCtx(() => [
+          createVNode(_component_RouterView, null, {
+            default: withCtx(({ Component }) => [
+              createVNode(Transition, {
+                name: "fade-fast",
+                mode: "out-in"
+              }, {
+                default: withCtx(() => [
+                  (openBlock(), createBlock(resolveDynamicComponent(Component), {
+                    key: unref(route).fullPath
+                  }))
+                ]),
+                _: 2
+              }, 1024)
+            ]),
+            _: 1
+          })
+        ]),
+        _: 1
+      });
+    };
+  }
+});
+if (typeof block0 === "function")
+  block0(_sfc_main);
+export { _sfc_main as default };
---
+++ public/vendeya/assets/sidebarLayoutState.f6df9299.js
@@ -0,0 +1,4 @@
+import { r as ref } from "./vendor.b09305a7.js";
+ref("default");
+const pageTitle = ref("Welcome");
+export { pageTitle as p };
