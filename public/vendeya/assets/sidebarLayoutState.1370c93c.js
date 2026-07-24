65f3ea4a021117418c9fcd471c932f7e12141810
Cristian Vega <cristianvega73212828@gmail.com>
2026-07-24 11:22:07 -0500

    feat(mozo/vendeya): actualización de compilados

--- public/vendeya/assets/sidebarLayoutState.1370c93c.js
+++
@@ -1,4 +0,0 @@
-import { r as ref } from "./vendor.80d481a8.js";
-ref("default");
-const pageTitle = ref("Welcome");
-export { pageTitle as p };
--- public/vendeya/assets/sidebarLayoutState.f6df9299.js
+++
@@ -1,4 +0,0 @@
-import { r as ref } from "./vendor.b09305a7.js";
-ref("default");
-const pageTitle = ref("Welcome");
-export { pageTitle as p };
