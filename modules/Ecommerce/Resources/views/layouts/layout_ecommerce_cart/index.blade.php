976a5c77183cfbf1df07e0a93927609314b882bb
angel espinoza <angel@buho.la>
2026-07-29 09:58:33 -0500

    Fix: mostrar formulario de Culqi en el checkout

--- modules/Ecommerce/Resources/views/layouts/layout_ecommerce_cart/index.blade.php
+++ modules/Ecommerce/Resources/views/layouts/layout_ecommerce_cart/index.blade.php
@@ -116,8 +116,7 @@
     <script src="{{ asset('porto-ecommerce/assets/js/jquery.min.js') }}"></script>
     <script src="{{ asset('porto-ecommerce/assets/js/bootstrap.bundle.min.js') }}"></script>
     <script src="{{ asset('porto-ecommerce/assets/js/plugins.min.js') }}"></script>
-    <!--<script src="{{ asset('porto-ecommerce/assets/js/culqi_v3.js') }}"></script>-->
-    <script src="https://checkout.culqi.com/js/v3"></script>
+    <script src="https://js.culqi.com/checkout-js"></script>
     <script src="{{ asset('porto-ecommerce/assets/js/sweetalert2.all.min.js') }}"></script>
     <script src="{{ asset('porto-ecommerce/assets/js/moment.min.js') }}"></script>
 
