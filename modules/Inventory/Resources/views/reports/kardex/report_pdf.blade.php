fde7de5c96a710effc2f00c9310b9bc5a734d6cc
Jenny-Santiago1 <jsantiagoalcantara1@gmail.com>
2026-09-04 12:15:19 -0500

    feat(reporte kardex): se agrega un label de anulación a guía anulada

--- modules/Inventory/Resources/views/reports/kardex/report_pdf.blade.php
+++ modules/Inventory/Resources/views/reports/kardex/report_pdf.blade.php
@@ -139,7 +139,18 @@
                         <td class="celda">
                             {{ \Carbon\Carbon::parse($itemKardex['date_time'])->format('d-m-Y g:i a') }}
                         </td>
-                        <td class="celda">{{$itemKardex['type_transaction']}}</td>
+                        <td class="celda">
+                            @php
+                                $typeTx = $itemKardex['type_transaction'] ?? '';
+                                $isVoidTx = is_string($typeTx) && str_contains($typeTx, '(Anulación)');
+                                $typeTxBase = $isVoidTx ? trim(preg_replace('/\s*\(Anulación\)\s*$/u', '', $typeTx)) : $typeTx;
+                            @endphp
+                            @if($isVoidTx)
+                                {{ $typeTxBase }} <span style="color:#dc3545;font-weight:bold;">(Anulación)</span>
+                            @else
+                                {{ $typeTx }}
+                            @endif
+                        </td>
                         <td class="celda">{{$itemKardex['number']}}</td>
                         <td class="celda">
                             {{$itemKardex['sale_note_asoc']}}
